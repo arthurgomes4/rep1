b56e5cea09e4b4bcf78136e02b94f0d79955e123
arthurgomes4 <55613848+arthurgomes4@users.noreply.github.com>
2019-09-22 17:30:10 +0530

    Add files via upload

---
+++ task prog 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+int main()
+{cout<<"enter the sentence";
+char ar[100],sr[20];
+gets(ar);
+puts(ar);
+cout<<"enter the string to be searched";
+gets(sr);
+
+
+}
